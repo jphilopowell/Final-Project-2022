c56cb94638cd76d7d92e8b41142ed158a1e0ad3e
jphilopowell <josephphilopowell.com>
2022-02-01 11:59:47 +0000

    updated bat entity and repository

--- src/main/java/com/example/cricketapp/data/repository/BatRepository.java
+++ src/main/java/com/example/cricketapp/data/repository/BatRepository.java
@@ -1,5 +1,11 @@
 package com.example.cricketapp.data.repository;
 
-public class BatRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.cricketapp.data.entity.Bat;
+
+@Repository
+public interface BatRepository extends JpaRepository<Bat, Long> {
 
 }
